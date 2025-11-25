3c98d301977489832aba833244e040bf9ae07b53
adriantizocmarshall <161913596+adriantizocmarshall@users.noreply.github.com>
2025-11-24 19:28:23 -0500

    Includes required project guidelines for concatenation, variables, etc.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+var A = "This is a string ";
+
+document.write (A);
+
+var B = 7;
+
+window.alert (B);
+
+var C = "I said, " + "\"I\'m not sure if this will work\"";
+
+document.write (C);
+
+var D = 5, E = 6, F = 7;
+
+//This is an example of an expression using declared variables.
+document.write (D + E + F);
+
+Sent1 = "This the beginning of the string";
+Sent2 = " and this is the end of the string";
+
+document.write (Sent1 + Sent2); //This is a concatenated string.
