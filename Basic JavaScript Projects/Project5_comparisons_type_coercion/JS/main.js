451384fbba1debb1846636aeb84bdd898d873faf
adriantizocmarshall <161913596+adriantizocmarshall@users.noreply.github.com>
2025-12-01 22:46:04 -0500

    Completed examples for typeof, coercion, infinity, boolean operators, console log methods, double equals, triple equals, AND, OR, and NOT operators

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,59 @@
+//Example of typeof operator returning "string"
+document.write(typeof "Hello World");
+
+//Example of coercion operator
+document.write("10" + 5);
+
+//Example of Infinity
+document.write(2E310);
+
+//Example of -Infinity
+document.write(-3E400);
+
+//Example of Boolean operator returning true
+document.write(10 > 5);
+
+//Example of Boolean operator returning false
+document.write(11 < 6);
+
+//Example of console log method
+console.log(5 + 5);
+
+//Console log method with boolean operator
+console.log(5 >= 5);
+
+//Console log method with double equals returning true
+console.log(true == true);
+
+//Console log method with double equals returning false
+console.log(true == false);
+
+//Example of triple equals returning true
+document.write(5 === 5);
+
+//Example of triple equals returning false with different data type and value
+document.write(3 === "2");
+
+//Example of triple equals returning false with different data type
+document.write("4" === 4);
+
+//Example of triple equals returning false with different value
+document.write(2 === 3);
+
+//Example of && AND operator returning true
+document.write(2 < 3 && 4 > 3);
+
+//Example of && AND operator returning false
+document.write(2 > 3 && 4 < 3);
+
+//Example of || OR operator returning true
+document.write(2 < 3 || 4 > 3);
+
+//Example of || OR operator returning false
+document.write(2 > 3 || 4 < 3);
+
+//Example of ! NOT operator returning false
+document.write(!(2 < 3));
+
+//Example of ! NOT operator returning true
+document.write(!(2 > 3));
